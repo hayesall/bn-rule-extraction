b07009a7ba6f9a8a39274d8b744c5a620a0d5c9f
hayesall <alexander@batflyer.net>
2021-01-06 11:00:53 -0500

    📝 Add notes and overview

--- rule_extraction.py
+++ rule_extraction.py
@@ -7,39 +7,34 @@ Extracting decision rules from Bayesian Networks
 from pomegranate import BayesianNetwork
 from pomegranate import DiscreteDistribution
 from pomegranate import ConditionalProbabilityTable
-
 from sklearn.preprocessing import OrdinalEncoder
-
 from sklearn.model_selection import cross_val_predict
 from sklearn.model_selection import LeaveOneOut
-
 from sklearn.metrics import accuracy_score
-
 import numpy as np
-import matplotlib.pyplot as plt
 
 
-names = np.loadtxt("toy_decision2.data", max_rows=1, delimiter=",", dtype=str)
-data = np.loadtxt("toy_decision2.data", skiprows=1, delimiter=",", dtype=str)
+names = np.loadtxt("toy_decision.csv", max_rows=1, delimiter=",", dtype=str)
+data = np.loadtxt("toy_decision.csv", skiprows=1, delimiter=",", dtype=str)
 
 enc = OrdinalEncoder(dtype=np.float32)
 data = enc.fit_transform(data)
+print(enc.categories_)
 
+# TODO(hayesall): ``mapping`` is basically a "pretty-printer", this could
+#       probably be included as part of the ``print_rules`` function.
 mapping = {}
-
 for variable_name, possible_values in zip(names, enc.categories_):
     for i, value_name in enumerate(possible_values):
 
         from_this = variable_name + " = " + str(float(i))
         to_that = variable_name + " = " + value_name
-        # print(from_this, "==>", to_that)
-
         mapping[from_this] = to_that
 
 y = data.T[0]
 X = data.T[1:].T
 
-def print_rules(pom_model):
+def print_rules(pom_model, variable_mapping):
 
     for i in range(len(model.states)):
 
@@ -47,9 +42,7 @@ def print_rules(pom_model):
             print(names[i], model.states[i].distribution.parameters)
 
         else:
-            # Assume Categorical
-            # print(names[i], model.structure[i], [names[j] for j in model.structure[i]])
-            # print(np.array(model.states[i].distribution.parameters[0]))
+            # Assume isinstance Categorical
 
             cpt = np.array(model.states[i].distribution.parameters[0])
             print("\n\n")
@@ -60,8 +53,8 @@ def print_rules(pom_model):
                 for j, par in enumerate([names[p] for p in model.structure[i]]):
 
                     seen = par + " = " + str(row[j])
-                    if seen in mapping:
-                        par_condition += mapping[seen]
+                    if seen in variable_mapping:
+                        par_condition += variable_mapping[seen]
                     else:
                         par_condition += seen
 
@@ -72,8 +65,8 @@ def print_rules(pom_model):
                 par_condition += ") THEN ("
 
                 seen = names[i] + " = " + str(row[-2])
-                if seen in mapping:
-                    par_condition += mapping[seen]
+                if seen in variable_mapping:
+                    par_condition += variable_mapping[seen]
                 else:
                     par_condition += seen
 
@@ -92,16 +85,7 @@ clf = BayesianNetwork()
 
 required = [
     tuple([1, 0]),
-    # tuple([3, 0]),
     tuple([4, 0]),
-]
-
-excluded = [
-    tuple([0, 0]),
-    tuple([0, 1]),
-    tuple([0, 2]),
-    tuple([0, 3]),
-    tuple([0, 4]),
 ]
 
 predictions = []
@@ -115,16 +99,15 @@ for train_index, test_index in loo.split(X):
     model = clf.from_samples(
         learning_data,
         algorithm='exact',
-        exclude_edges=excluded,
         include_edges=required,
         state_names=[str(name) for name in names],
         max_parents=-1,
-        # pseudocount=1.0,
     )
     print(model.structure)
 
-    # print_rules(model)
-    # import pdb; pdb.set_trace()
+    if test_index == 0:
+        print("Decision rules extracted from the first test:\n")
+        print_rules(model, mapping)
 
     nan_column = np.empty(y_test.shape)
     nan_column[:] = np.nan
@@ -132,13 +115,8 @@ for train_index, test_index in loo.split(X):
 
     pred = model.predict_proba(test_data)
 
-    # print(pred[0][0])
-
     predictions.append(
         [item[0].items()[1][1] > 0.5 for item in pred][0]
     )
-    # print(predictions)
-
-# import pdb; pdb.set_trace()
 
 print(accuracy_score(np.array(predictions), y))
